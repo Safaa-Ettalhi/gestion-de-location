712c0a8ad6e7f9086f9a9741050402eff688a40b
Safaa-Ettalhi <safaeettalhi@gmail.com>
2024-12-09 11:14:35 +0100

    connection de base de donner

--- db.php
+++ db.php
@@ -1,13 +1,16 @@
 <?php
-$host = 'localhost';     // L'hôte de la base de données
-$dbname = 'location_voitures';  // Nom de la base de données
-$username = 'root';      // Votre nom d'utilisateur
-$password = 'safaa';          // Votre mot de passe (s'il y en a un)
+$host = 'localhost';    
+$dbname = 'location_voitures';  
+$username = 'root';      
+$password = 'safaa';     
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Erreur de connexion : " . $e->getMessage();
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Erreur de connexion : " . $conn->connect_error);
 }
+
+$conn->set_charset("utf8");
+
 ?>
