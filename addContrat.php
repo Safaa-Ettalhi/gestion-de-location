a98d9cc8f9c4928cf3e144cfbe312f81ed4d6632
Safaa-Ettalhi <safaeettalhi@gmail.com>
2024-12-13 00:12:05 +0100

    umeloaration de code dajout de contrat

--- addContrat.php
+++ addContrat.php
@@ -1,16 +1,18 @@
 <?php
 include('db.php');
 if( $_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])){
-    $id_client = trim(htmlspecialchars($_POST['id_client']));
-    $id_voiture = trim(htmlspecialchars($_POST['id_voiture']));
-    $date_debut = trim(htmlspecialchars($_POST['date_debut']));
-    $date_fin = trim(htmlspecialchars($_POST['date_fin']));
-    $total = trim(htmlspecialchars($_POST['total']));
-    $statut = trim(htmlspecialchars($_POST['statut']));
+    $id_client = $_POST['id_client'];
+    $id_voiture = $_POST['id_voiture'];
+    $date_debut = $_POST['date_debut'];
+    $date_fin = $_POST['date_fin'];
+    $total = $_POST['total'];
+    $statut = $_POST['statut'];
 
 
 $query = $conn->query("INSERT INTO `contrats` (`id_client`, `id_voiture`, `date_debut`, `date_fin`, `total`,`statut`) 
                                 VALUES ('$id_client', '$id_voiture', '$date_debut', '$date_fin', '$total','$statut')");
+   
+
     header("Location: ./contrat.php");
 
 }
